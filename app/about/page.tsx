e942720477aba25eb7b77c8b6e927a5bcf5e1ab8
Pauline Bihler <pauline.erika5@gmail.com>
2023-12-13 16:37:57 +0100

    Add navbar styling

---
+++ app/about/page.tsx
@@ -0,0 +1,14 @@
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'About | Reading Log',
+  description: 'Keep track of your reads',
+};
+
+export default function AboutPage() {
+  return (
+    <div>
+      <h1> About Reading Log</h1>
+    </div>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -6,8 +6,9 @@ export default function Home() {
   return (
     <main>
       <div>
-        <h1>Hello World !</h1>
-        <h2>Home Page</h2>
+        {/* <h1>Hello World !</h1> */}
+        {/* <h1 className="text-3xl font-bold underline">Hello world!</h1>
+        <h2>Home Page</h2> */}
       </div>
     </main>
   );
